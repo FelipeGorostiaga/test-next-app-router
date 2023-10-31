4832bafe9f732a41902fcd333dbefa8e8990761e
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-10-31 13:53:34 -0300

    New changes!

---
+++ app/ui/GaliciaLogo.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const GaliciaLogo = () => {
+  return (
+    <Image src={'/download.png'} alt={'Galicia logo'} width={200} height={58} />
+  );
+}
+
+export default GaliciaLogo;
